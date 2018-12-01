fc6c5374a3bb5752193210db8462cf24025a0990
r.j <articlemming@gmail.com>
2018-12-01 13:48:13 -0500

    buildCommand function is a work in progress. You can proceed working under the assumption that it works and supplies the command structure as advertised.

--- main.c
+++ main.c
@@ -40,6 +40,8 @@ int main()
     head = (myNode *)malloc(sizeof(myNode));
     head = NULL;
     memset(arglist, 0, sizeof(arglist));
+    commands_t command_list;
+    int thepipe[2]; /// Holds two fd's for piping.
 
     while (q)
     {
@@ -57,10 +59,49 @@ int main()
                 *holder = 0;
             }
         }
+
+        /// Scan input for pipes
+        int pipeCount = scanForPipes(argbuf);
+        /// Pipes were found.
+        /// 1) Build a list of the commands that need to be executed.
+        /// 2) Create pipe.
+        /// 3) Run modified linCommand
+        if(pipeCount > 0){
+            command_list.command = (char ***) calloc((size_t) pipeCount + 1, sizeof(char **));
+            command_list.count = pipeCount + 1;
+            command_list.numOfArgs = (int *) calloc((size_t) pipeCount + 1, sizeof(int));
+            /// @mmeadwell22 I am still trying to get this function to properly organize the user input.
+            /// you can proceed with the writing the code and assume that it works.
+
+            buildCommandList(&command_list, argbuf);
+            createPipe(thepipe);
+            /// Loop through each command in the command list and set the pipe flags accordingly.
+            for(int j = 0; j < command_list.count; j++){
+                if(j == 0){ /// Only redirect stdout
+                    //linCommand(command_list.command[j], head, command_list.numOfArgs[j], thepipe, 0, 1);
+                }
+                else if (j == 1 && command_list.count == 3 ){ /// Redirect both stdin & stdout
+                    //linCommand(command_list.command[j], head, command_list.numOfArgs[j], thepipe, 1, 1);
+                }
+                else if( j == 1 && command_list.count == 2){ /// Only redirect stdin
+                    //linCommand(command_list.command[j], head, command_list.numOfArgs[j], thepipe, 1, 0);
+                }
+                else if( j == 2){ /// Only redirect stdin
+                    //linCommand(command_list.command[j], head, command_list.numOfArgs[j], thepipe, 1, 0);
+                }
+            }
+
+        }
+        else if (pipeCount == -1){
+            /// Hanging pipe character detected.
+            printf("please re-try your command");
+            break;
+        }
+
         arglist[0] = strtok(argbuf, " ");
         numOfArgs = 1;
         strcpy(command, arglist[0]);
-        //first argument cotains command
+        //first argument contains command
         i = 1;
         //rest of the command line arguments
         int tempcount = 0;
@@ -97,7 +138,7 @@ int main()
             else
             {
                 int total = numOfArgs - tempcount;
-                linCommand(arglist, head, total);
+                linCommand(arglist, head, total, thepipe, 0, 0);
             }
         }
     }
